4d3abe747e9478a9b697e1c989cd6d1d1783b6c9
AlexB <axb19c@acu.edu>
2022-11-21 21:14:33 -0600

    Initial Commit

---
+++ swift_roll/swift_rollApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_rollApp.swift
+//  swift_roll
+//
+//  Created by Alexander Burgos on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_rollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift_rollUITests/swift_rollUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift_rollUITestsLaunchTests.swift
+//  swift_rollUITests
+//
+//  Created by Alexander Burgos on 11/21/22.
+//
+
+import XCTest
+
+final class swift_rollUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
